32776322690022392ed1e6b36732b0a457d18622
Burhan AYDIN <aydinburhan@outlook.com.tr>
2022-11-25 14:04:41 +0300

    emre

--- basicobj/src/components/Car.js
+++ basicobj/src/components/Car.js
@@ -1,33 +1,3 @@
-// import React, { useState } from 'react'
-
-// export default function Car() {
-//     const [car, setCar] = useState({
-//         brand : "Ford",
-//         model : "Mustang",
-//         year : "1964",
-//         color : "red"
-//     });
-
-//     const updateModel = (e) => {
-//         let m = e.target.value
-//         //alert(m)
-//          setCar(previousState => {
-//             return {...previousState, model : m}
-//         })
-//     }
-//   return (
-//     <div>
-//         <h3>My {car.brand}</h3>
-//         <p>
-//             It is a {car.color} {car.model} from {car.year}.
-//         </p>
-//         <input type="text" onKeyUp={(event) => {
-//             updateModel(event)
-//         }} />
-//         {/* <button type="button" onClick={() => updateModel("Fiesta")}>Update Model</button> */}
-//     </div>
-//   )
-// }
 
 
 import React, { useState } from 'react'
